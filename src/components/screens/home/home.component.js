5778888d73dc369e96ec78395e0d628f7ec6fbde
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-27 08:17:42 +0300

    lessons up to 20 (5943)

---
+++ src/components/screens/home/home.component.js
@@ -0,0 +1,11 @@
+import { BaseScreen } from '@/core/component/base-screen.component';
+
+export class Home extends BaseScreen {
+	constructor() {
+		super({ title: 'Home' });
+	}
+
+	render() {
+		return '<p>Home</p>';
+	}
+}
