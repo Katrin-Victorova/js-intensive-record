59d32f2d2411e004fd535fa537d6ca523620f30f
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-05-28 23:16:00 +0300

    Lesson 22

--- src/core/services/render.service.js
+++ src/core/services/render.service.js
@@ -12,6 +12,10 @@ class RenderService {
 		template.innerHTML = html.trim();
 		const element = template.content.firstChild;
 
+		if (styles) {
+			this.#applyModuleStyles(styles, element);
+		}
+
 		this.#replaceComponentTags(element, components);
 
 		return element;
@@ -54,7 +58,31 @@ class RenderService {
 			}
 		}
 	}
-	// #applyModuleStyles
+
+	/**
+	 * @param {Object} moduleStyles
+	 * @param {string} element
+	 * @param {void}
+	 */
+	#applyModuleStyles(moduleStyles, element) {
+		if (!element) return;
+
+		const applyStyles = element => {
+			for (const [key, value] of Object.entries(moduleStyles)) {
+				if (element.classList.contains(key)) {
+					element.classList.remove(key);
+					element.classList.add(value);
+				}
+			}
+		};
+
+		if (element.getAttribute('class')) {
+			applyStyles(element);
+		}
+
+		const elements = element.querySelectorAll('*');
+		elements.forEach(applyStyles);
+	}
 }
 
 export default new RenderService();
