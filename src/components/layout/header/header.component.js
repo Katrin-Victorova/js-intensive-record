1351c7fd565c73df7ccb361759d4a144d10781a9
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-06-18 14:06:24 +0300

    27 - header

--- src/components/layout/header/header.component.js
+++ src/components/layout/header/header.component.js
@@ -1,12 +1,38 @@
 import ChildComponent from '@/core/component/child.component'
 import renderService from '@/core/services/render.service'
 
+import { UserItem } from '@/components/ui/user-item/user-item.component'
+
 import styles from './header.module.scss'
 import template from './header.template.html'
 
+import { Logo } from './logo/logo.component'
+import { LogoutButton } from './logout-button/logout-button.component'
+import { Search } from './search/search.component'
+
 export class Header extends ChildComponent {
+	constructor({ router }) {
+		super()
+		this.router = router
+	}
+
 	render() {
-		this.element = renderService.htmlToElement(template, [], styles)
+		this.element = renderService.htmlToElement(
+			template,
+			[
+				Logo,
+				new LogoutButton({
+					router: this.router
+				}),
+				Search,
+				new UserItem({
+					avatarPath:
+						'https://images.steamusercontent.com/ugc/2461858129090424341/094EAEA386FD1C45EA85EDC10690C988E3DA9332/',
+					name: 'Kat'
+				})
+			],
+			styles
+		)
 
 		return this.element
 	}
