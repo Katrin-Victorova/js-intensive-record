59aa0bd4df6a055702c57dfc480cce4871f27070
Katrin Viktorova <katyadubinchuk@mail.ru>
2025-06-20 09:54:25 +0300

    29 servise

---
+++ src/core/services/notification.service.js
@@ -0,0 +1,51 @@
+/**
+ * NotificationService is a utility class to handle displaying notifications.
+ * It can bee used to display messages with different types (success, error)
+ * and manage the notification timeout.
+ */
+import styles from '@/components/layout/notification/notification.module.scss'
+
+import { $R } from '../rquery/rquery.lib'
+
+export class NotificationService {
+	#timeout
+
+	constructor() {
+		this.#timeout = null
+	}
+
+	#setTimeout(callback, duration) {
+		if (this.#timeout) {
+			clearTimeout(this.#timeout)
+		}
+		this.#timeout = setTimeout(callback, duration)
+	}
+	/**
+	 * Show a notification with a specified message and type.
+	 * The notification will automatically hide after a specified duration.
+	 * @param {string} message - The message to be displayed in the notification.
+	 * @param {('success' | 'error')} type - The type of the notification,
+	 * only 'success' or 'error' are accepted.
+	 */
+	show(type, message) {
+		if (!['success', 'error'].includes(type)) {
+			throw new Error(
+				'Invalid notification type. Allowed types are "success" and "error".'
+			)
+		}
+
+		const classNames = {
+			success: styles.success,
+			error: styles.error
+		}
+
+		const notificationElement = $R('#notification')
+		const className = classNames[type]
+
+		notificationElement.text(message).addClass(className)
+
+		this.#setTimeout(() => {
+			notificationElement.removeClass(className)
+		}, 3000)
+	}
+}
